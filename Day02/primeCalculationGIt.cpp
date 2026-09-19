7b6a0851509a654bc55e1d10cb583e839f94604b
sahupriyanshu368-gif <sahupriyanshu368@gmail.com>
2026-09-19 22:49:40 +0530

    Add Day02 prime calculation and update readme

---
+++ Day02/primeCalculationGIt.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number you want to check whether it is prime or not : ";
+    cin >> n;
+
+    bool isPrime = true;
+    if (n<=1) {
+        isPrime = false;
+    }
+
+   
+
+   
+
+   for (int i=2; i*i<=n; i++) {
+
+     if (n%i==0) {
+        isPrime = false;
+        break;
+    }
+   }
+
+
+   if (isPrime) {
+    cout << n << " is a Prime Number" << endl;
+   } else {
+    cout << n << " is not a Prime Number" << endl;
+   }
+
+    return 0;
+}
